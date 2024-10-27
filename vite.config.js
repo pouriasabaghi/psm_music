354affadd15bb9b9d95b72dc9fe96f8f1d564639
Pouria Sabaghi <pouria.782@gmail.com>
2024-10-27 12:10:45 +0330

    cleanup

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
+import eslint from 'vite-plugin-eslint';
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), eslint()],
 })
