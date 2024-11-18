7cacb4fe86d6be49a4d9e865b5b09372510834e3
Pouria Sabaghi <pouria.782@gmail.com>
2024-11-18 19:35:05 +0330

    🧑‍💻 chore: no songs message

--- src/features/songs/SongList.jsx
+++ src/features/songs/SongList.jsx
@@ -1,14 +1,25 @@
+import { Link } from "react-router-dom";
 import SongItem from "./SongItem";
 import { useSongs } from "./useSongs";
-
+import { Button } from "@/ui/button";
 
 function SongList() {
-  const {songs, isPending} = useSongs();
+  const { songs, isPending } = useSongs();
 
-  if(isPending) return <div>Loading...</div>
+  if (isPending) return <div>Loading...</div>;
+
+  if (songs.length === 0)
+    return (
+      <div>
+        <p>You didn't add any song, try uploading one</p>
+        <Button className="mt-5" asChild>
+          <Link to={"/songs/upload"}>Upload song</Link>
+        </Button>
+      </div>
+    );
+
   return (
     <div className="space-y-4" role="list">
-     
       {songs.map((song) => (
         <SongItem key={song.id} song={song} />
       ))}
