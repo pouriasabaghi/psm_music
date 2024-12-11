f5c198b04bd09c6d3c92fdf5bdaf35cad003f052
pouriasabaghi <pouriasabaghi@gmail.com>
2024-12-11 15:40:48 +0330

    🐛 fix: fixing top song play function

--- src/features/songs/TopSongs.jsx
+++ src/features/songs/TopSongs.jsx
@@ -1,15 +1,16 @@
+import { usePlayerController } from "@/context/PlayerControllerContext";
 import { useTopSongs } from "./useTopSongs";
 import SongItem from "@/features/songs/SongItem";
 
 
 function TopSongs() {
   const { topSongs, isPending } = useTopSongs();
-
+  const { play, stop } = usePlayerController();
   if (isPending) return <p>Loading...</p>;
   return (
     <div className="space-y-4" role="list">
       {topSongs.map((song) => (
-        <SongItem key={song.id} song={song} />
+        <SongItem key={song.id} song={song} play={play} stop={stop} />
       ))}
     </div>
   );
